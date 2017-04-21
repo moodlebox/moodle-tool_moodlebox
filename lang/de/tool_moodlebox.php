0b109ec7a4087f76cd7c3854c48af3ef0c3b1f37
Nicolas Martignoni <nicolas@martignoni.net>
2017-04-21 10:42:10 +0200

    Language file for de added, courtesy of Ralf Krause

---
+++ lang/de/tool_moodlebox.php
@@ -0,0 +1,66 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'tool_moodlebox', language 'de' (German)
+ *
+ * @package    tool_moodlebox
+ * @copyright  2016 onwards Nicolas Martignoni <nicolas@martignoni.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['changepassworderror'] = 'Das MoodleBox-Kennwort wurde nicht geändert, weil die eingegebenen Kennwörter nicht übereinstimmen.';
+$string['changepasswordmessage'] = 'Das MoodleBox-Kennwort für das Systemkonto (Unix) wurde geändert.<br /><br />Achtung! Das Kennwort für das Moodle-Nutzerkonto \'admin\' <b>wurde nicht verändert</b>. Öffnen Sie dafür die Einstellungen für das Moodle-Nutzerkonto \'admin\'.';
+$string['changepasswordsetting'] = 'MoodleBox-Kennwort ändern';
+$string['changewifipassword'] = 'WLAN-Kennwort ändern';
+$string['cpufrequency'] = 'CPU-Frequenz';
+$string['cpuload'] = 'CPU-Auslastung';
+$string['cputemperature'] = 'CPU-Temperatur';
+$string['currentwifipassword'] = 'WLAN-Kennwort';
+$string['datetime'] = 'Datum und Zeit';
+$string['datetimemessage'] = 'Die Systemzeit in der MoodleBox wurde eingestellt. Um eine bestmögliche Genauigkeit zu erreichen, sollten Sie die MoodleBox über ein Ethernetkabel mit dem Internet verbinden.';
+$string['datetimeset'] = 'Systemzeit einstellen';
+$string['datetimesetmessage'] = 'Die Systemzeit in der MoodleBox ist nicht richtig eingestellt. Sie sollten dringend das aktuelle Datum und die richtige Zeit einstellen.';
+$string['datetimesetting'] = 'Systemzeit einstellen';
+$string['dhcpclientinfo'] = 'IP-Adresse und Name';
+$string['dhcpclientnumber'] = 'DHCP-Clients';
+$string['information'] = 'Informationen';
+$string['kernelversion'] = 'Kernel-Version';
+$string['missingconfigurationerror'] = 'Dieser Bereich ist nicht verfügbar. Die Installation des Plugins ist nicht vollständig, so dass keine Einstellungen für die MoodleBox vorgenommen werden können. Um den Fehler zu beheben, lesen Sie die <a href="https://github.com/martignoni/make-moodlebox/blob/master/doc/Moodlebox.pdf" target="_blank">Dokumentation zur MoodleBox</a>.';
+$string['moodleboxpluginversion'] = 'Plugin-Version';
+$string['newwifipassword'] = 'Neues WLAN-Kennwort';
+$string['nopassworddefined'] = 'Kein WLAN-Kenwort festgelegt';
+$string['parameter'] = 'Parameter';
+$string['pluginname'] = 'MoodleBox';
+$string['raspberryhardware'] = 'Hardware';
+$string['raspbianversion'] = 'Betriebssystem';
+$string['restart'] = 'MoodleBox neustarten';
+$string['restartmessage'] = 'Die MoodleBox wird neugestartet. Das System wird nach einer kurzen Unterbrechung wieder online sein.';
+$string['restartstop'] = 'Neustarten und ausschalten';
+$string['rpi1'] = 'Raspberry Pi 1';
+$string['rpi2'] = 'Raspberry Pi 2 Modell B';
+$string['rpi3'] = 'Raspberry Pi 3 Modell B';
+$string['sdcardavailablespace'] = 'Freier Speicher';
+$string['shutdown'] = 'MoodleBox ausschalten';
+$string['shutdownmessage'] = 'Die MoodleBox wird ausgeschaltet. Warten Sie mehrere Sekunden, bevor Sie die Spannungsversorgung entfernen.';
+$string['systeminfo'] = 'Systeminformationen';
+$string['unsupportedhardware'] = 'Die Hardware des Servers wird nicht unterstützt! Dieses Plugin arbeitet ausschließlich mit einem Raspberry Pi.';
+$string['uptime'] = 'Betriebsdauer';
+$string['wifipassworderror'] = 'Das WLAN-Kennwort muss zwischen 8 und 63 Zeichen lang sein.';
+$string['wifipasswordmessage'] = 'Das WLAN-Kennwort wurde geändert. Denken Sie daran, Ihren Teilnehmer/innen die Änderung mitzuteilen.';
+$string['wifipasswordsetting'] = 'WLAN-Kennwort ändern';
