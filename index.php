88efd3b99901a28ae71d2038c0346db70adde6e8
Nicolas Martignoni <nicolas@martignoni.net>
2017-04-30 12:22:57 +0200

    Hardware test refactored

--- index.php
+++ index.php
@@ -58,18 +58,20 @@ if ( $cpuinfo = file_get_contents('/proc/cpuinfo') ) {
 }
 
 switch ( $hardware ) {
-    case 'BCM2708':
+    case 'BCM2708': // RPi 1.
         $platform = 'rpi1';
         break;
-    case 'BCM2709':
-    case 'BCM2835':
+    case 'BCM2709': // RPi 2 or RPi 3 on kernel earlier than 4.9.x.
         if ( $revision === 'a02082' || $revision === 'a22082' ) {
             $platform = 'rpi3';
         } else {
             $platform = 'rpi2';
         }
         break;
-    default:
+    case 'BCM2835': // RPi 3 on kernel 4.9.x or later.
+        $platform = 'rpi3';
+        break;
+    default: // Anything else is not a RPi.
         $platform = 'unknownmodel';
 }
 
