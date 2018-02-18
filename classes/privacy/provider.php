0849708cc959593c5575e4a100606904101959cd
Nicolas Martignoni <nicolas@martignoni.net>
2018-02-18 13:44:52 +0100

    Implementation of Moodle privacy API, issue #34.

---
+++ classes/privacy/provider.php
@@ -0,0 +1,37 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Privacy Subsystem implementation for tool_moodlebox.
+ *
+ * @package    tool_moodlebox
+ * @copyright  2018 onwards Nicolas Martignoni <nicolas@martignoni.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace tool_moodlebox\privacy;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Privacy Subsystem for tool_moodlebox implementing null_provider.
+ * This plugin does not store any personal user data.
+ *
+ * @copyright  2018 onwards Nicolas Martignoni <nicolas@martignoni.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class provider implements \core_privacy\metadata\null_provider {
+}
