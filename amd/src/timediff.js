bb5672993e401f4a3a8c79df1ee4c5621fdaac0c
Nicolas Martignoni <nicolas@martignoni.net>
2018-07-09 12:02:34 +0200

    Revert some secure links to cope with Moodle coding style.

--- amd/src/timediff.js
+++ amd/src/timediff.js
@@ -11,7 +11,7 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /*
  * @see        https://github.com/moodlebox/moodle-tool_moodlebox
