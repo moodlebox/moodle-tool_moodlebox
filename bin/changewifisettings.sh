a3f02c6f9cc406e535dbd94deab98ae920bc1df1
Nicolas Martignoni <nicolas@martignoni.net>
2018-11-11 21:39:28 +0100

    Script updated for SSID hiding feature
    - See issue #56.

--- bin/changewifisettings.sh
+++ bin/changewifisettings.sh
@@ -31,6 +31,7 @@ NEWCOUNTRY="$(grep '^country\b' $FILE | cut -d= -f2)"
 NEWPASSWORD="$(grep '^password\b' $FILE | cut -d= -f2)"
 NEWSSID="$(grep '^ssid\b' $FILE | cut -d= -f2)"
 PASSWORDPROTECTED="$(grep '^passwordprotected\b' $FILE | cut -d= -f2)"
+SSIDHIDDEN="$(grep '^ssidhiddenstate\b' $FILE | cut -d= -f2)"
 # Valid country codes. See https://www.iso.org/iso-3166-country-codes.html.
 ALLOWEDCOUNTRIES="AD AE AF AG AI AL AM AO AQ AR AS AT AU AW AX AZ BA BB BD BE BF BG BH BI BJ BL BM BN BO BQ BR BS BT BV BW BY BZ CA CC CD CF CG CH CI CK CL CM CN CO CR CU CV CW CX CY CZ DE DJ DK DM DO DZ EC EE EG EH ER ES ET FI FJ FK FM FO FR GA GB GD GE GF GG GH GI GL GM GN GP GQ GR GS GT GU GW GY HK HM HN HR HT HU ID IE IL IM IN IO IQ IR IS IT JE JM JO JP KE KG KH KI KM KN KP KR KW KY KZ LA LB LC LI LK LR LS LT LU LV LY MA MC MD ME MF MG MH MK ML MM MN MO MP MQ MR MS MT MU MV MW MX MY MZ NA NC NE NF NG NI NL NO NP NR NU NZ OM PA PE PF PG PH PK PL PM PN PR PS PT PW PY QA RE RO RS RU RW SA SB SC SD SE SG SH SI SJ SK SL SM SN SO SR SS ST SV SX SY SZ TC TD TF TG TH TJ TK TL TM TN TO TR TT TV TW TZ UA UG UM US UY UZ VA VC VE VG VI VN VU WF WS YE YT ZA ZM ZW"
 #
@@ -71,6 +72,18 @@ sed -i "/^ssid=/c\ssid2=$NEWSSID" "$CONFIGFILE"
 sed -i "/^ssid2=/c\ssid2=$NEWSSID" "$CONFIGFILE"
 if [[ -z $(grep "^utf8_ssid=1$" "$CONFIGFILE") ]]; then # add utf8_ssid param.
     sed -i "/ssid2/a utf8_ssid=1" "$CONFIGFILE"
+fi
+#
+# SSID hiding setting.
+# Validate SSID hiding setting. Replace it with 0 if invalid.
+[[ $SSIDHIDDEN =~ ^[01]$ ]] || SSIDHIDDEN="0"
+# SSID hiding setting is now valid; set it in config file.
+# Check if line "ignore_broadcast_ssid=..." exist uncommented in config file.
+STRING="^ignore_broadcast_ssid=\b"
+if [ -z $(grep "$STRING" "$CONFIGFILE") ]; then # Line not found, so we write it at end of file.
+    sed -i "\$a# Show or hide SSID\nignore_broadcast_ssid=$SSIDHIDDEN" "$CONFIGFILE"
+else # Line found, we change the setting as needed.
+    sed -i "/^ignore_broadcast_ssid=/c\ignore_broadcast_ssid=$SSIDHIDDEN" "$CONFIGFILE"
 fi
 #
 # Password protection setting.
