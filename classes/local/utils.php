34ac7e082036581ff6b9b37d1e6b97867d1d0c48
Nicolas Martignoni <nicolas@martignoni.net>
2019-06-25 21:59:26 +0200

    Enable RPi 4B detection
    - Issue #70.

--- classes/local/utils.php
+++ classes/local/utils.php
@@ -56,20 +56,20 @@ class utils {
      * |1098|7654|3210|9876|5432|1098|7654|3210|
      * +----+----+----+----+----+----+----+----+
      *
-     * +---+-------+-------------–-+--------------------------------------------+
-     * | # | bits  | contains      | values                                     |
-     * +---+-------+------------–--+--------------------------------------------+
-     * | A | 00-03 | PCB Revision  | The PCB revision number                    |
-     * | B | 04-11 | Model name    | A, B, A+, B+, 2B, Alpha, CM1, unknown, 3B, |
-     * |   |       |               | Zero, CM3, unknown, Zero W, 3B+            |
-     * | C | 12-15 | Processor     | BCM2835, BCM2836, BCM2837                  |
-     * | D | 16-19 | Manufacturer  | Sony UK, Egoman, Embest, Sony Japan,       |
-     * |   |       |               | Embest, Stadium                            |
-     * | E | 20-22 | Memory size   | 256 MB, 512 MB, 1024 MB                    |
-     * | F | 23-23 | Revision flag | (if set, new-style revision)               |
-     * | G | 24-24 | Warranty bit  | (if set, warranty void - Pre Pi2)          |
-     * | H | 25-25 | Warranty bit  | (if set, warranty void - Post Pi2)         |
-     * +---+-------+---------------+--------------------------------------------+
+     * +---+-------+-------------–-+----------------------------------------------------+
+     * | # | bits  | contains      | values                                             |
+     * +---+-------+------------–--+----------------------------------------------------+
+     * | A | 00-03 | PCB Revision  | The PCB revision number                            |
+     * | B | 04-11 | Model name    | A, B, A+, B+, 2B, Alpha, CM1, unknown, 3B, Zero,   |
+     * |   |       |               | CM3, unknown, Zero W, 3B+, 3A+, internal, CM3+, 4B |
+     * | C | 12-15 | Processor     | BCM2835, BCM2836, BCM2837, BCM2711                 |
+     * | D | 16-19 | Manufacturer  | Sony UK, Egoman, Embest, Sony Japan,               |
+     * |   |       |               | Embest, Stadium                                    |
+     * | E | 20-22 | Memory size   | 256 MB, 512 MB, 1 GB, 2 GB, 4 GB                   |
+     * | F | 23-23 | Revision flag | (if set, new-style revision)                       |
+     * | G | 24-24 | Warranty bit  | (if set, warranty void - Pre Pi2)                  |
+     * | H | 25-25 | Warranty bit  | (if set, warranty void - Post Pi2)                 |
+     * +---+-------+---------------+----------------------------------------------------+
      *
      * @return associative array of parameters, value or false if unsupported hardware.
      */
@@ -86,10 +86,11 @@ class utils {
         $revisionnumber = hexdec($revisionnumber);
 
         // Define arrays of various hardware parameter values.
-        $memorysizes = array('256', '512', '1024');
+        $memorysizes = array('256 MB', '512 MB', '1 GB', '2 GB', '4 GB');
         $models = array('A', 'B', 'A+', 'B+', '2B', 'Alpha', 'CM1', 'Unknown',
-                '3B', 'Zero', 'CM3', 'Unknown', 'ZeroW', '3B+');
-        $processors = array('BCM2835', 'BCM2836', 'BCM2837');
+                '3B', 'Zero', 'CM3', 'Unknown', 'ZeroW', '3B+', '3A+', 'Internal use',
+                'CM3+', '4B');
+        $processors = array('BCM2835', 'BCM2836', 'BCM2837', 'BCM2711');
         $manufacturers = array('Sony UK', 'Egoman', 'Embest', 'Sony Japan',
                 'Embest', 'Stadium');
 
