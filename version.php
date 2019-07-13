01a566d180a23c03fc97e0ac8103201ed17fecea
Nicolas Martignoni <nicolas@martignoni.net>
2019-07-13 22:05:15 +0200

    Update version date and number for upcoming release 2.2.1

--- version.php
+++ version.php
@@ -26,8 +26,8 @@ defined('MOODLE_INTERNAL') || die;
 
 $plugin = new stdClass();
 
-$plugin->version  = 2019071000;
-$plugin->release = '2.2.0';
+$plugin->version  = 2019071200;
+$plugin->release = '2.2.1';
 $plugin->requires = 2018120300;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'tool_moodlebox';
