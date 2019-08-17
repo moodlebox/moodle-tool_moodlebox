683c010af0ded915137e72a8ac502ddb9b1cec3f
Nicolas Martignoni <nicolas@martignoni.net>
2019-08-17 14:34:24 +0200

    Update version number and date

--- version.php
+++ version.php
@@ -26,8 +26,8 @@ defined('MOODLE_INTERNAL') || die;
 
 $plugin = new stdClass();
 
-$plugin->version  = 2019081700;
-$plugin->release = '2.4.0-dev';
+$plugin->version  = 2019081701;
+$plugin->release = '2.4.0';
 $plugin->requires = 2018120300;
-$plugin->maturity = MATURITY_BETA;
+$plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'tool_moodlebox';
