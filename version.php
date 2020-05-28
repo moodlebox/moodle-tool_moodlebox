6bef4c403d683e2426eebde0153372a3c9b3ecbc
Nicolas Martignoni <nicolas@martignoni.net>
2020-05-28 11:40:30 +0200

    Bump version id

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die;
 
 $plugin = new stdClass();
 
-$plugin->version  = 2020051900;
+$plugin->version  = 2020052800;
 $plugin->release = '2.6.0dev';
 $plugin->requires = 2018120300;
 $plugin->supported = [36, 39];
