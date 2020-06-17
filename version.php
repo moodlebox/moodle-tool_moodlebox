d42ddfcff597e3df5d35fbbdbaf1248851d08330
Nicolas Martignoni <nicolas@martignoni.net>
2020-06-17 18:34:32 +0200

    Prepare next version

--- version.php
+++ version.php
@@ -26,9 +26,9 @@ defined('MOODLE_INTERNAL') || die;
 
 $plugin = new stdClass();
 
-$plugin->version  = 2020060800;
-$plugin->release = '2.6.0';
+$plugin->version  = 2020061700;
+$plugin->release = '2.7.0-dev';
 $plugin->requires = 2018120300;
 $plugin->supported = [36, 39];
-$plugin->maturity = MATURITY_STABLE;
+$plugin->maturity = MATURITY_BETA;
 $plugin->component = 'tool_moodlebox';
