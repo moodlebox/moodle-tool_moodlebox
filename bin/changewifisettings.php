41c1a617174ade06dc375619394d17ea583a3f7e
Nicolas Martignoni <nicolas@martignoni.net>
2021-07-14 22:46:21 +0200

    Continue implementation of script in PHP
    - Still work in progress.

--- bin/changewifisettings.php
+++ bin/changewifisettings.php
@@ -1,7 +1,7 @@
 #!/usr/bin/env php
 <?php
 // This script is part of MoodleBox plugin for moodlebox
-// Copyright (C) 2016 onwards Nicolas Martignoni
+// Copyright (C) 2021 onwards Nicolas Martignoni
 //
 // This script is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -22,11 +22,23 @@ if (posix_getuid() !== 0){
   exit(1);
 }
 
-function file_replace_line($file_name, $parameter_to_change, $parameter_to_replace){
+function file_replace_line($file_name, $parameter_to_change, $parameter_to_replace) {
     $file = explode("\n", rtrim(file_get_contents($file_name)));
     $file = preg_replace('/^' . $parameter_to_change . '$/', $parameter_to_replace, $file);
     $file = implode("\n", $file);
     file_put_contents($file_name, $file);
+}
+
+function file_insert_line_after($file_name, $line_to_detect, $line_to_insert) {
+    $file = explode("\n", rtrim(file_get_contents($file_name)));
+    $file = preg_replace('/^' . $line_to_detect . '$/', '\0' . "\n" . $line_to_insert, $file);
+    $file = implode("\n", $file);
+    file_put_contents($file_name, $file);
+}
+
+function is_regex_in_file($file_name, $regex) {
+    $file = explode("\n", rtrim(file_get_contents($file_name)));
+    return count(preg_filter($regex, '\0', $file)) !== 0;
 }
 
 // Configuration.
@@ -52,7 +64,7 @@ $new_password = $settings['password'];
 $new_ssid = $settings['ssid'];
 $password_protected = $settings['passwordprotected'];
 $ssid_hidden = $settings['ssidhiddenstate'];
-$new_static_id = $settings['ipaddress'];
+$new_static_ip = $settings['ipaddress'];
 
 // Valid country codes {@link https://www.iso.org/iso-3166-country-codes.html}.
 $allowed_countries = "AD AE AF AG AI AL AM AO AQ AR AS AT AU AW AX AZ BA BB BD BE BF BG BH BI BJ BL BM BN BO BQ BR BS BT BV BW BY BZ CA CC CD CF CG CH CI CK CL CM CN CO CR CU CV CW CX CY CZ DE DJ DK DM DO DZ EC EE EG EH ER ES ET FI FJ FK FM FO FR GA GB GD GE GF GG GH GI GL GM GN GP GQ GR GS GT GU GW GY HK HM HN HR HT HU ID IE IL IM IN IO IQ IR IS IT JE JM JO JP KE KG KH KI KM KN KP KR KW KY KZ LA LB LC LI LK LR LS LT LU LV LY MA MC MD ME MF MG MH MK ML MM MN MO MP MQ MR MS MT MU MV MW MX MY MZ NA NC NE NF NG NI NL NO NP NR NU NZ OM PA PE PF PG PH PK PL PM PN PR PS PT PW PY QA RE RO RS RU RW SA SB SC SD SE SG SH SI SJ SK SL SM SN SO SR SS ST SV SX SY SZ TC TD TF TG TH TJ TK TL TM TN TO TR TT TV TW TZ UA UG UM US UY UZ VA VC VE VG VI VN VU WF WS YE YT ZA ZM ZW";
@@ -101,68 +113,72 @@ preg_match_all("/^(?:[0-9a-fA-F]{2}){1,32}$/", $new_ssid, $results);
 if ( empty($results[0]) || $new_ssid != $results[0][0] ) {
     $new_ssid = '4d6f6f646c65426f78';
 }
-// New SSID is now valid; set it in config file.
-// We change ssid to ssid2 too
+// New SSID is now valid; set it in config file. We change ssid to ssid2 too.
 file_replace_line($hostapd_conf_file, 'ssid2?=.*', 'ssid2=' . $new_ssid);
-var_dump($new_ssid);
+// Check if hostapd config file defines a 'utf8_ssid' key.
+if ( !is_regex_in_file($hostapd_conf_file, '/^utf8_ssid=\b/') ) { // add utf8_ssid param.
+    file_insert_line_after($hostapd_conf_file, 'ssid2=.*', 'utf8_ssid=1');
+}
 
-/*
-sed -i "/^ssid=/c\ssid2=$NEWSSID" "$HOSTAPDCONFIGFILE"
-sed -i "/^ssid2=/c\ssid2=$NEWSSID" "$HOSTAPDCONFIGFILE"
-if [[ -z $(grep "^utf8_ssid=1$" "$HOSTAPDCONFIGFILE") ]]; then # add utf8_ssid param.
-    sed -i "/ssid2/a utf8_ssid=1" "$HOSTAPDCONFIGFILE"
-fi
-//
 // SSID hiding setting.
 // Validate SSID hiding setting. Replace it with 0 if invalid.
-[[ $SSIDHIDDEN =~ ^[01]$ ]] || SSIDHIDDEN="0"
+if ( $ssid_hidden != 0 && $ssid_hidden != 1 ) {
+    $ssid_hidden = 0;
+}
 // SSID hiding setting is now valid; set it in config file.
 // Check if line "ignore_broadcast_ssid=..." exist uncommented in config file.
-STRING="^ignore_broadcast_ssid=\b"
-if [ -z $(grep "$STRING" "$HOSTAPDCONFIGFILE") ]; then # Line not found, so we write it at end of file.
-    sed -i "\$a# Show or hide SSID\nignore_broadcast_ssid=$SSIDHIDDEN" "$HOSTAPDCONFIGFILE"
-else # Line found, we change the setting as needed.
-    sed -i "/^ignore_broadcast_ssid=/c\ignore_broadcast_ssid=$SSIDHIDDEN" "$HOSTAPDCONFIGFILE"
-fi
-//
+if ( !is_regex_in_file($hostapd_conf_file, '/^ignore_broadcast_ssid=\b/') ) {
+    file_put_contents($hostapd_conf_file, "\n# Show or hide SSID\nignore_broadcast_ssid=" . $ssid_hidden, FILE_APPEND);
+} else {
+    file_replace_line($hostapd_conf_file, 'ignore_broadcast_ssid=.*', 'ignore_broadcast_ssid=' . $ssid_hidden);
+}
+
 // Password protection setting.
-// Validate password protection setting. Replace it with 1 if invalid.
-[[ $PASSWORDPROTECTED =~ ^[01]$ ]] || PASSWORDPROTECTED="1"
+// Validate and convert password protection setting to boolean. Set it to 'true' if invalid.
+if ( $password_protected != 0 && $password_protected != 1 ) {
+    $password_protected = true;
+} else {
+    $password_protected = ($password_protected === '1');
+}
 // Check if line "wpa_passphrase=..." exist uncommented in config file.
 // If found, the Wi-Fi network is currently password protected.
-STRING="^wpa_passphrase=\b"
-if [ -z $(grep "$STRING" "$HOSTAPDCONFIGFILE") ]; then # Line not found, we're not password protected.
-    ISCURRENTLYPROTECTED=false
-else # Line found, we're password protected.
-    ISCURRENTLYPROTECTED=true
-fi
-if [ "$PASSWORDPROTECTED" -eq 1 ]; then
-    if [[ "$ISCURRENTLYPROTECTED" == false ]]; then
-        # echo "Would like to protect and is NOT protected"
-        sed -i "/#*wpa_passphrase=/c\wpa_passphrase=$NEWPASSWORD" "$HOSTAPDCONFIGFILE"
-        sed -i "/#*wpa=/c\wpa=2" "$HOSTAPDCONFIGFILE"
-        sed -i "/#*wpa_key_mgmt=/c\wpa_key_mgmt=WPA-PSK" "$HOSTAPDCONFIGFILE"
-        sed -i "/#*rsn_pairwise=/c\rsn_pairwise=CCMP" "$HOSTAPDCONFIGFILE"
-    fi
-else
-    if [[ "$ISCURRENTLYPROTECTED" == true ]]; then
-        # echo "Would NOT like to protect but is protected"
-        sed -i "/^wpa_passphrase=/c\#wpa_passphrase=moodlebox" "$HOSTAPDCONFIGFILE"
-        sed -i "/^wpa=/c\#wpa=2" "$HOSTAPDCONFIGFILE"
-        sed -i "/^wpa_key_mgmt=/c\#wpa_key_mgmt=WPA-PSK" "$HOSTAPDCONFIGFILE"
-        sed -i "/^rsn_pairwise=/c\#rsn_pairwise=CCMP" "$HOSTAPDCONFIGFILE"
-    fi
-fi
-//
+$is_currently_protected = is_regex_in_file($hostapd_conf_file, '/^wpa_passphrase=\b/');
+if (!$password_protected && $is_currently_protected) {
+    file_replace_line($hostapd_conf_file, '^wpa_passphrase=.*', '# wpa_passphrase=moodlebox');
+    file_replace_line($hostapd_conf_file, '^wpa=.*', '# wpa=2');
+    file_replace_line($hostapd_conf_file, '^wpa_key_mgmt=.*', '# wpa_key_mgmt=WPA-PSK');
+    file_replace_line($hostapd_conf_file, '^rsn_pairwise=.*', '# rsn_pairwise=CCMP');
+} else if ($password_protected && !$is_currently_protected ) {
+    file_replace_line($hostapd_conf_file, '^#.*wpa_passphrase=.*', 'wpa_passphrase=' . $new_password);
+    file_replace_line($hostapd_conf_file, '^#.*wpa=.*', 'wpa=2');
+    file_replace_line($hostapd_conf_file, '^#.*wpa_key_mgmt=.*', 'wpa_key_mgmt=WPA-PSK');
+    file_replace_line($hostapd_conf_file, '^#.*rsn_pairwise=.*', 'rsn_pairwise=CCMP');
+}
+
 // Static IP setting.
-// Validate IP address. Replace it with '10.0.0.1' if invalid, reserved or public.
-// RFC 1918 {@link https://datatracker.ietf.org/doc/html/rfc1918#section-3 }.
+// Validate IP address. RFC 1918 {@link https://datatracker.ietf.org/doc/html/rfc1918#section-3 }.
 // Regex shamelessly taken from {@link https://stackoverflow.com/a/44333761/}.
-[[ $NEWSTATICIP =~ ^(10(\.(25[0-5]|2[0-4][0-9]|1[0-9]{1,2}|[0-9]{1,2})){3}|((172\.(1[6-9]|2[0-9]|3[01]))|192\.168)(\.(25[0-5]|2[0-4][0-9]|1[0-9]{1,2}|[0-9]{1,2})){2})$ ]] || NEWSTATICIP="10.0.0.1"
-// New static IP is now valid; compute DHCP range and set ip and range in all needed files.
-sed -i "/^static ip_address=/c\static ip_address=$NEWSTATICIP/24" "$DHCPCDCONFIGFILE"
-sed -i "/^listen-address=(?!127)/c\listen-address=$NEWSTATICIP" "$DNSMASQCONFIGFILE"
-sed -i "/^address=\/home\//c\address=\/home\/$NEWSTATICIP" "$DNSMASQCONFIGFILE"
+preg_match_all("/^(?:10(\.(25[0-5]|2[0-4][0-9]|1[0-9]{1,2}|[0-9]{1,2})){3}|((172\.(1[6-9]|2[0-9]|3[01]))|192\.168)(\.(25[0-5]|2[0-4][0-9]|1[0-9]{1,2}|[0-9]{1,2})){2})$/", $new_static_ip, $results);
+// Replace it with '10.0.0.1' if invalid, reserved or public.
+if ( empty($results[0]) || $new_static_ip != $results[0][0] ) {
+    $new_static_ip = '10.0.0.1';
+} else { // Ensure last segment of IP is 1.
+    $ip = explode('.', $new_static_ip);
+    array_pop($ip);
+    array_push($ip, 1);
+    $new_static_ip = implode('.', $ip);
+}
+// New static IP is now valid; compute DHCP range.
+$ip = explode('.', $new_static_ip);
+$subnet = implode('.', array_slice($ip, 0, count($ip)-1));
+$min_range = $subnet . '.10';
+$max_range = $subnet . '.254';
+// Set IP and range in all needed files.
+file_replace_line($dhcpcd_conf_file, 'static ip_address=.*', 'static ip_address=' . $new_static_ip . '/24');
+file_replace_line($dnsmasq_conf_file, 'listen-address=(?!127).*', 'listen-address=' . $new_static_ip);
+file_replace_line($dnsmasq_conf_file, 'address=\/home\/.*', 'address=/home/' . $new_static_ip);
+
+/*
 // End of actions.
 //
 // Restart hostapd service.
